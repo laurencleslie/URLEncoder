352b36860f2c4c93405acddb4c1659a99da04434
laurencleslie <42555185+laurencleslie@users.noreply.github.com>
2018-09-21 17:32:48 -0500

    Add files via upload

---
+++ Encoder.cs
@@ -0,0 +1,32 @@
+using System;
+
+namespace URLEncoder
+{
+    class Program
+    {
+    
+        public static void Main(string[] args)
+        {
+        string project;
+        string activity;
+
+        Console.WriteLine("Enter project name: ");
+        project=Console.ReadLine();
+
+        Console.WriteLine("Enter activity name: ");
+        activity=console.ReadLine();
+
+        Console.WriteLine("The Following URL: \n");
+        Console.WriteLine("https://companuserver.com/Content/ " +project+ "%20/files/" +activity "Report.pdf");
+
+
+            }
+
+        }
+    }
+}
+")
+
+        }
+    }
+}
